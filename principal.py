f229f4c221baf1bd3f09b789d98149f2c1e45852
[TonyCozu] <[tonyflacocozu@gmail.com]>
2022-11-21 11:42:20 -0600

    commit inicial

---
+++ principal.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Tony Titulo")
+
+st.header("Encabezado numero 1")
+
+st.subheader("subencabezado numero 1")
+
+st.write("Tony")
+
+st.markdown("*Italica*, **negrita** [Enlace Pagina del curso](https://gf0657-programacionsig.github.io/2022-ii/contenido/5/streamlit.html)")
